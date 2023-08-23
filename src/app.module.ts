beaa8b1625a2d0a69323937233369b1416dd81d4
Arsen <abdigal.2015@gmail.com>
2023-08-24 01:00:39 +0600

    finished app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+	EventController,
+	FightController,
+	FighterController
+} from 'src/controllers';
+import { DataServicesModule } from './services/data-services/data-services.module';
+import { EventUseCasesModule } from './use-cases/event/event-use-cases.module';
+import { FightUseCasesModule } from './use-cases/fight/fight-use-cases.module';
+import { FighterUseCasesModule } from './use-cases/fighter/fighter-use-cases.module';
+import { FighterPersonalDataUseCasesModule } from './use-cases/fighterPersonalData/fighterPersonalData-use-cases.module';
+import { FighterStatsUseCasesModule } from './use-cases/fighterStats/fighterStats-use-cases.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		DataServicesModule,
+		EventUseCasesModule,
+		FightUseCasesModule,
+		FighterUseCasesModule,
+		FighterPersonalDataUseCasesModule,
+		FighterStatsUseCasesModule
+	],
+	controllers: [
+		EventController,
+		FightController,
+		FighterController
+	],
+	providers: []
 })
-export class AppModule {}
+export class AppModule { }
