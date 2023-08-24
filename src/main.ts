941cc8c4ff0bd4e2e9f0ba87b049c1df191ef220
Arsen <abdigal.2015@gmail.com>
2023-08-25 03:00:30 +0600

    configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('STARFUND API')
+		.setDescription('The STARFUND API for MMA fights')
+		.setVersion('1.0')
+		.addTag('starfund')
+		.build();
+	const document = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(3000);
 }
+
 bootstrap();
