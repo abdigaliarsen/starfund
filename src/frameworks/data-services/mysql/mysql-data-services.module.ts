1deb98023733a202b100a5b88ab86968c02f8155
Arsen <abdigal.2015@gmail.com>
2023-08-24 18:13:12 +0600

    added data source option

--- src/frameworks/data-services/mysql/mysql-data-services.module.ts
+++ src/frameworks/data-services/mysql/mysql-data-services.module.ts
@@ -4,18 +4,11 @@ import { DATA_BASE_CONFIGURATION } from 'src/configuration';
 import { MySQLDataServices } from './mysql-data-services.service';
 import { Event, Fight, Fighter, FighterPersonalData, FighterStats } from './model';
 import { IDataServices } from 'src/core';
+import { dataSourceOptions } from '@/configuration/data-source';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: DATA_BASE_CONFIGURATION.mysqlHost,
-            port: parseInt(DATA_BASE_CONFIGURATION.mysqlPort),
-            username: DATA_BASE_CONFIGURATION.mysqlUser,
-            password: DATA_BASE_CONFIGURATION.mysqlPassword,
-            database: DATA_BASE_CONFIGURATION.mysqlDatabase,
-            entities: [Event, Fight, Fighter, FighterPersonalData, FighterStats]
-        }),
+        TypeOrmModule.forRoot(dataSourceOptions),
         TypeOrmModule.forFeature([Event, Fight, Fighter, FighterPersonalData, FighterStats])
     ],
     providers: [{
