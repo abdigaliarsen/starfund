c35ebb10c5500254ddc4cc55dbc130ec86368386
Arsen <abdigal.2015@gmail.com>
2023-08-24 18:12:34 +0600

    create data source

---
+++ src/configuration/data-source.ts
@@ -0,0 +1,18 @@
+import { Event, Fight, Fighter, FighterPersonalData, FighterStats } from "../frameworks/data-services/mysql/model";
+import { DATA_BASE_CONFIGURATION } from ".";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'mysql',
+    host: DATA_BASE_CONFIGURATION.mysqlHost,
+    port: parseInt(DATA_BASE_CONFIGURATION.mysqlPort),
+    username: DATA_BASE_CONFIGURATION.mysqlUser,
+    password: DATA_BASE_CONFIGURATION.mysqlPassword,
+    database: DATA_BASE_CONFIGURATION.mysqlDatabase,
+    entities: [Event, Fight, Fighter, FighterPersonalData, FighterStats],
+    migrations: ['src/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
