e66f1198ee7cf5a5f4547138dc9bf4c3b7a9b6b2
Arsen <abdigal.2015@gmail.com>
2023-08-24 18:12:56 +0600

    exports

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from "./event.controller";
 export * from "./fight.controller";
 export * from "./fighter.controller";
+export * from "./fighterPersonalData.controller";
+export * from "./fighterStats.controller";
