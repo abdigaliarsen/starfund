77e6ee0cb465770da915d04b70ca84c708ea4bf6
Arsen <abdigal.2015@gmail.com>
2023-08-25 08:30:48 +0600

    export

---
+++ src/core/abstracts/specific-repositories/index.ts
@@ -0,0 +1,6 @@
+export * from './event-repository.abstract';
+export * from './fight-repository.abstract';
+export * from './fighter-repository.abstract';
+export * from './fighterPersonalData-repository.abstract';
+export * from './fighterStats-repository.abstract';
+export * from './fightersFights-repository.abstract';
